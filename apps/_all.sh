e95e51590315beb8d51fd312586a9b434734152a
Patrick Boyle <t.patrick.boyle@pm.me>
2025-09-09 17:49:15 -0500

    cleanup

--- apps/_all.sh
+++ apps/_all.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 
 DOCKER_APPS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 source "$DOCKER_APPS_DIR/docker.sh"
 
 provision_all_docker_apps () {
-  docker_is_provisioned
+  ensure_docker_is_provisioned
 }
--- apps/docker.sh
+++ apps/docker.sh
@@ -1,19 +1,32 @@
+#!/bin/bash
+
+
+# SOURCE
 
 source "src/packages/apt.sh"
 source "src/services.sh"
 
+
+# CONSTANTS
+
 APT_REPO_URL="https://download.docker.com/linux/ubuntu"
 APT_KEY_URL="$APT_REPO_URL/gpg"
 
-docker_is_provisioned () {
-  docker_requirements_are_installed
+
+# INTERFACE
+
+ensure_docker_is_provisioned () {
+  ensure_docker_requirements_are_installed
   apt_ensure_key_is_installed docker "$APT_KEY_URL"
   apt_ensure_sources_file_exists docker "$APT_REPO_URL"
-  docker_packages_are_installed
+  ensure_docker_packages_are_installed
   ensure_service_is_active docker
 }
 
-docker_requirements_are_installed () {
+
+# IMPLEMENTATION
+
+ensure_docker_requirements_are_installed () {
   apt_ensure_packages_are_installed \
     apt-transport-https \
     ca-certificates \
@@ -21,7 +34,7 @@ docker_requirements_are_installed () {
     software-properties-common
 }
 
-docker_packages_are_installed () {
+ensure_docker_packages_are_installed () {
   apt_ensure_packages_are_installed \
     docker-ce \
     docker-ce-cli \
